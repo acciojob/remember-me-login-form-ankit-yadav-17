aa670d87efa69e4b4b25fb6ac401519e89ef1550
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-21 12:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function() {
+      // Check if there are saved details in local storage
+      var savedUsername = localStorage.getItem('username');
+      var savedPassword = localStorage.getItem('password');
+      
+      if (savedUsername && savedPassword) {
+        var existingButton = document.createElement('button');
+        existingButton.id = 'existing';
+        existingButton.textContent = 'Login as existing user';
+        existingButton.addEventListener('click', function() {
+          alert('Logged in as ' + savedUsername);
+        });
+        document.body.appendChild(existingButton);
+      }
+    });
+
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+      var rememberMe = document.getElementById('rememberMe').checked;
+      
+      if (rememberMe) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+      } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+      }
+      
+      alert('Logged in as ' + username);
+    });
